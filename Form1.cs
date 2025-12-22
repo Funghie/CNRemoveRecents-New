0d1fceed7df01ee0a86c85fdbf7f696dd859322b
Phil Pendlebury <phil@everythingcreative.biz>
2025-12-22 16:50:30 +0400

    Code Cleanup

--- Form1.cs
+++ Form1.cs
@@ -1,13 +1,14 @@
-﻿using System;
+﻿// Copyright (c) 2025 Phil Pendlebury
+// Everything Creative
+// Licensed under MIT
+
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.IO;
+using System.Linq;
+using System.Windows.Forms;
 using System.Xml.Linq;
 
 namespace CNRemoveRecents
